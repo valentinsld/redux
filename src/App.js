753845f664df2d8cd7e90942a280571c421c6c90
valentin sld <valentin.salaud@gmail.com>
2020-11-18 16:51:28 +0100

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { useDispatch, useSelector } from "react-redux";
+import {
+  useDispatch,
+  useSelector
+} from 'react-redux';
 
-import "./App.css";
+import { set_number } from './actions/actions-types';
 
-function App() {
-  const { message } = useSelector((state) => state);
+const App = () => {
+  const { message, number} = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const handleChange = e => {
+    const { value : number } = e.target;
+
+    dispatch(set_number(number));
+  }
 
   return (
     <div className="App">
       <p>{message}</p>
+      <p>Number : {number}</p>
+      <input type="text" value={number} name="number" onChange={handleChange} />
     </div>
   );
 }
